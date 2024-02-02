edb1b130f7958c320cf70ea069adb2d7e1f6778f
hooiv <125276621+hooiv@users.noreply.github.com>
2024-02-01 23:25:03 -0800

    new file:   src/App.css 	new file:   src/App.js 	deleted:    src/app.js 	new file:   src/components/navbar.jsx 	new file:   src/index.js

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import "./App.css";
+import {BrowserRouter as Router, Routes,Route} from "react-router-dom"
+import {Nw}
+function App(){
+    return (<div className="App">
+        <Router>
+            <Routes>
+                <Route path="/"  />
+                <Route path="/cart" />
+            </Routes>
+        </Router>
+    </div>
+    );
+}
+export default App;
