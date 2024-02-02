6644229526ffbf3b03c2e23d6f9ff42fca0be2eb
hooiv <125276621+hooiv@users.noreply.github.com>
2024-02-02 10:23:59 +0000

    Feb 2, 2024, 2:23 AM

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -2,6 +2,10 @@ import React from 'react'
 
 export const Navbar = () => {
   return (
-    <div>n</div>
+    <div className='navbar'>
+     <div className="links">
+      
+     </div>
+    </div>
   )
 }
