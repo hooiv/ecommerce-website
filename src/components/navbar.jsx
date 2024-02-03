14231ed68397c005a377a778640ed0606bac0111
hooiv <125276621+hooiv@users.noreply.github.com>
2024-02-03 11:34:11 +0000

    Feb 3, 2024, 11:34 AM

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,11 +1,15 @@
 import React from 'react'
+import {Link} from "react-router-dom";
+import {ShoppingCart} from "phosphor-react";
 
 export const Navbar = () => {
   return (
     <div className='navbar'>
      <div className="links">
-       <Link> Shop </Link>
-       <Link></Link>
+       <Link to="/"> Shop </Link>
+       <Link to="/cart">
+        <ShoppingCart size={32} />
+       </Link>
       
      </div>
     </div>
