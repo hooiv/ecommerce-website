4e835acfc28ec627d2b3da3a158266e48c54b5ef
hooiv <125276621+hooiv@users.noreply.github.com>
2024-07-09 13:29:54 -0700

    modified:   src/context/shop-context.jsx

--- src/context/shop-context.jsx
+++ src/context/shop-context.jsx
@@ -13,5 +13,5 @@ export const ShopContextProvider=(props)=>{
     const [cartItems,setCartItems]=useState(getDefautCart());
 const addToCart=(itemId)=>
     set cartItems((prev)=>({...pr ev, [itemId]:prev[itemId]+1}))
-    return <ShopContettttttttttttttttttttttttttttttttttxt.ShopContextProvider>{props.children}</ShopContext.ShopContextProvider>
+    return <ShopContext.ShopContextProvider>{props.children}</ShopContext.ShopContextProvider>
 };
