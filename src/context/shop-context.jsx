44f2906c89d1e56e3687bf73a9649813d887b2bc
hooiv <125276621+hooiv@users.noreply.github.com>
2024-07-15 15:33:51 +0000

    15 Jul 2024, 21:03

--- src/context/shop-context.jsx
+++ src/context/shop-context.jsx
@@ -1,14 +1,14 @@
 import {createContext,useEffect,useState} from "react";
 import { PRODUCTS } from "../products";
 
-export const ShopContext=createContext(null);
+export const ShopContext=createContext(null);;
 const getDefautCart=()=>{
     let cart={}
     for (let i=1;i<PRODUCTS.length+1;i++){
         cart[i]=0;
     }
     return cart;
-};
+};;
 export const ShopContextProvider=(props)=>{
     const [cartItems,setCartItems]=useState(getDefautCart());
 
