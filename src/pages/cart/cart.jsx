71b48a595d6555c19bcf9740cf326207325349cb
hooiv <125276621+hooiv@users.noreply.github.com>
2024-07-15 17:01:37 +0000

    15 Jul 2024, 22:31

--- src/pages/cart/cart.jsx
+++ src/pages/cart/cart.jsx
@@ -3,6 +3,7 @@ import { PRODUCTS } from '../../products'
 import { ShopContext } from '../../context/shop-context';
 import {CartItem} from './cart-item';
 import {useNavigate} from "react-router-dom";
+import "./cart.css";
 export const Cart = () => {
   const {cartItems,getTotalCartAmount,checkout}=useContext(ShopContext);
   const totalAmount=getTotalCartAmount();
