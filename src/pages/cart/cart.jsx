99efa4d3a52895d94804d8ddb339004647e373ae
hooiv <125276621+hooiv@users.noreply.github.com>
2024-07-15 16:31:26 +0000

    15 Jul 2024, 22:01

--- src/pages/cart/cart.jsx
+++ src/pages/cart/cart.jsx
@@ -1,8 +1,9 @@
-import React , {useContext} from 'react';
-import { PRODUCTS } from '../../products'
-import { ShopContext } from '../../context/shop-context';
-import {CartItem} from './cart-item';
-import {useNavigate} from "react-router-dom";
+import React, { useContext } from "react";
+import { ShopContext } from "../../context/shop-context";
+import { PRODUCTS } from "../../products";
+import { CartItem } from "./cart-item";
+import { useNavigate } from "react-router-dom";
+
 export const Cart = () => {
   const {cartItems,getTotalCartAmount,checkout}=useContext(ShopContext);
   const totalAmount=getTotalCartAmount();
