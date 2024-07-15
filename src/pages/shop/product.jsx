7528f4b81b2c31d35e603fe5ba3e92c26ab42b64
hooiv <125276621+hooiv@users.noreply.github.com>
2024-07-15 16:23:04 +0000

    15 Jul 2024, 21:53

--- src/pages/shop/product.jsx
+++ src/pages/shop/product.jsx
@@ -2,7 +2,7 @@ import React ,{useContext} from "react";
 import { ShopContext } from "../../context/shop-context"; 
 export const Product=(props) => {
     const {id,productName,price,productImage}=props.data;
-       const {addToCart,cartItems}=useContext(ShopContext);
+    const {addToCart,cartItems}=useContext(ShopContext);
     const cartItemAmount=cartItems[id];
 
     return ( <div className="product">
