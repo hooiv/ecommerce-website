d79e123b6a15c6d7c8b3989410d433edef5a81e4
hooiv <125276621+hooiv@users.noreply.github.com>
2024-07-15 13:33:27 +0000

    15 Jul 2024, 19:03

--- src/pages/shop/product.jsx
+++ src/pages/shop/product.jsx
@@ -1,6 +1,10 @@
 import React from "react";
+import { ShopContext } from "../../context/shop-context"; 
 export const Product=(props) => {
     const {id,productName,price,productImage}=props.data;
+
+
+
     return ( <div className="product">
         <img src={productImage}/>
         <div className="description">
